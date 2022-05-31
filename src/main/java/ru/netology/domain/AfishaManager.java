609f11dede86cfa16b3bce8007290cd4e5787b91
irina godonoage <godonoage@bk.ru>
2022-05-31 15:57:52 +0300

    one commit

---
+++ src/main/java/ru/netology/domain/AfishaManager.java
@@ -0,0 +1,49 @@
+package ru.netology.domain;
+
+public class AfishaManager {
+
+    private Movie[] movies = new Movie[0];
+    private int numberFilm = 10;
+
+    public AfishaManager() {
+    }
+
+    public AfishaManager(int numberFilm) {
+        this.numberFilm = numberFilm;
+    }
+
+    public void add(Movie object) {
+
+        int length = movies.length + 1;
+        Movie[] tmp = new Movie[length];
+        System.arraycopy(movies, 0, tmp, 0, movies.length);
+        int lastIndex = tmp.length - 1;
+        tmp[lastIndex] = object;
+        movies = tmp;
+    }
+
+    public Movie[] findAll() {
+        Movie[] result = new Movie[movies.length];
+        for (int i = 0; i < result.length; i++) {
+            int index = movies.length - i - 1;
+            result[i] = movies[index];
+        }
+        return result;
+    }
+
+    public Movie[] findLast() {
+        int resultLength;
+        if (movies.length > numberFilm) {
+            resultLength = numberFilm;
+        } else {
+            resultLength = movies.length;
+        }
+
+        Movie[] result = new Movie[resultLength];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = movies[movies.length - i - 1];
+        }
+        return result;
+    }
+
+}
---
+++ src/test/java/ru/netology/domain/AfishaManagerTest.java
@@ -0,0 +1,105 @@
+package ru.netology.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AfishaManagerTest {
+    Movie first = new Movie(1, "Бладшот", "Боевик");
+    Movie second = new Movie(2, "Вперед", "Мультфильм");
+    Movie third = new Movie(3, "Отель Белград", "Комедия");
+    Movie fourth = new Movie(4, "Джентельмены", "Боевик");
+    Movie fifth = new Movie(5, "Человек-невидимка", "Ужасы");
+    Movie sixth = new Movie(6, "Тролли. Мировой тур", "Мультфильм");
+    Movie seventh = new Movie(7, "Номер Один", "Комедия");
+    Movie eighth = new Movie(8, "Наемник", "Боевик");
+    Movie ninth = new Movie(9, "Хочу замуж", "Комедия");
+    Movie tenth = new Movie(10, "Лулу и Бриггс", "Комедия");
+    Movie eleven = new Movie(11, "Парни под прикрытием", "Боевик");
+
+
+
+    @Test
+    void addFilm() {
+        AfishaManager afishaManager = new AfishaManager(0);
+        afishaManager.add(first);
+        Movie[]expected = new Movie[] {first};
+        Movie[]actual = afishaManager.findAll();
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void findAll() {
+        AfishaManager afishaManager = new AfishaManager(4);
+        afishaManager.add(first);
+        afishaManager.add(second);
+        afishaManager.add(third);
+        afishaManager.add(fourth);
+        Movie[] expected = new Movie[]{fourth,third,second,first};
+        Movie[]actual = afishaManager.findAll();
+        assertArrayEquals(expected,actual);
+
+
+    }
+    @Test
+    void findAll2() {
+        AfishaManager afishaManager = new AfishaManager(1);
+        afishaManager.add(first);
+
+
+        Movie[] expected = new Movie[] { first };
+        Movie[] actual = afishaManager.findAll();
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void findLast() {
+        AfishaManager afishaManager = new AfishaManager(5);
+        afishaManager.add(first);
+        afishaManager.add(second);
+        afishaManager.add(third);
+        afishaManager.add(fourth);
+        afishaManager.add(fifth);
+        afishaManager.add(sixth);
+        afishaManager.add(seventh);
+
+        Movie[] expected = new Movie[] {seventh, sixth, fifth, fourth, third };
+        Movie[] actual = afishaManager.findLast();
+
+        assertArrayEquals(expected, actual);
+    }
+    @Test
+    void shouldFindLastThanMoreNumberOfFilm() {
+        AfishaManager afishaManager = new AfishaManager();
+        afishaManager.add(first);
+        afishaManager.add(second);
+        afishaManager.add(third);
+        afishaManager.add(fourth);
+        afishaManager.add(fifth);
+        afishaManager.add(sixth);
+        afishaManager.add(seventh);
+        afishaManager.add(eighth);
+        afishaManager.add(ninth);
+        afishaManager.add(tenth);
+        afishaManager.add(eleven);
+
+        Movie[] expected = new Movie[] {eleven, tenth, ninth, eighth, seventh, sixth, fifth, fourth, third, second };
+        Movie[] actual = afishaManager.findLast();
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void shouldFindLastThanLessNumberOfFilm() {
+        AfishaManager afishaManager = new AfishaManager(5);
+        afishaManager.add(first);
+        afishaManager.add(second);
+        afishaManager.add(third);
+
+        Movie[] expected = new Movie[] { third, second, first };
+        Movie[] actual = afishaManager.findLast();
+
+        assertArrayEquals(expected, actual);
+    }
+}
